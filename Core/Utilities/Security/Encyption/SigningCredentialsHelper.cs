c146b51adc5b20b5abf691b153801579e2dfd2d9
Selim Kazancı <selimkznci@gmail.com>
2021-12-07 04:07:08 +0300

    Signing security

--- Core/Utilities/Security/Encyption/SigningCredentialsHelper.cs
+++ Core/Utilities/Security/Encyption/SigningCredentialsHelper.cs
@@ -4,8 +4,10 @@ namespace Core.Utilities.Security.Encyption
 {
     public class SigningCredentialsHelper
     {
+        //'Signing=İmzalama' = Token servislerimizi web api'de jwt tokenlarının oluşturulması için kullanırız 
         public static SigningCredentials CreateSigningCredentials(SecurityKey securityKey)
         {
+            //Bu anahtarı kullan algoritma olarakta şifreleme olarakta HmacSha512Signature
             return new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha512Signature);
         }
     }
