add8995fbb4ce293849a5754d6e73011373f6b23
Selim Kazancı <selimkznci@gmail.com>
2021-12-09 04:01:15 +0300

    CoreModule

--- Core/DependencyResolvers/CoreModule.cs
+++ Core/DependencyResolvers/CoreModule.cs
@@ -1,4 +1,6 @@
-﻿using Core.Utilities.IoC;
+﻿using Core.Cross_Cutting_Concers.Caching;
+using Core.Cross_Cutting_Concers.Caching.Microsoft;
+using Core.Utilities.IoC;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using System;
@@ -13,7 +15,10 @@ namespace Core.DependencyResolvers
     {
         public void Load(IServiceCollection serviceCollection)
         {
+            serviceCollection.AddMemoryCache(); //MemoryCacheManager    //
             serviceCollection.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+            serviceCollection.AddSingleton<ICacheManager, MemoryCacheManager>();//MemoryCacheManager    
+            //serviceCollection.AddSingleton<ICacheManager, RedisCacheManager>();   Redise geçersek 20.satır silinir redis gelir addmemoryCache silinir
         }
     }
 }
