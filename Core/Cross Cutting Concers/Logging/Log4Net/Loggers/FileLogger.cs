a01e9c8b28385437451ce322d65be28b3addddba
Selim Kazancı <selimkznci@gmail.com>
2021-12-26 02:57:42 +0300

    Log4Net

---
+++ Core/Cross Cutting Concers/Logging/Log4Net/Loggers/FileLogger.cs
@@ -0,0 +1,9 @@
+﻿namespace Core.Cross_Cutting_Concers.Logging.Log4Net.Loggers
+{
+    public class FileLogger : LoggerServiceBase
+    {
+        public FileLogger() : base("JsonFileLogger")
+        {
+        }
+    }
+}
---
+++ Core/Cross Cutting Concers/Logging/Log4Net/SerializableLogEvent.cs
@@ -0,0 +1,22 @@
+﻿using log4net.Core;
+using System;
+
+namespace Core.Cross_Cutting_Concers.Logging.Log4Net
+{
+    public partial class LoggerServiceBase
+    {
+        [Serializable]
+        public class SerializableLogEvent
+        {
+            private LoggingEvent _loggingEvent;
+
+            public SerializableLogEvent(LoggingEvent loggingEvent)
+            {
+                _loggingEvent = loggingEvent;
+            }
+
+            public object Message => _loggingEvent.MessageObject;
+        }
+
+    }
+}
