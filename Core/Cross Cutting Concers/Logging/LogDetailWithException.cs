7053ec14cdf969709ccc7461378cc8be567796bb
Selim Kazancı <selimkznci@gmail.com>
2021-12-26 02:58:04 +0300

    CCC Logging

---
+++ Core/Cross Cutting Concers/Logging/LogDetailWithException.cs
@@ -0,0 +1,8 @@
+﻿namespace Core.Cross_Cutting_Concers.Logging
+{
+    public class LogDetailWithException : LogDetail
+    {
+        public string ExceptionMessage { get; set; }
+    }
+
+}
---
+++ Core/Cross Cutting Concers/Logging/LogParameter.cs
@@ -0,0 +1,9 @@
+﻿namespace Core.Cross_Cutting_Concers.Logging
+{
+    public class LogParameter
+    {
+        public string Name { get; set; }
+        public object Value { get; set; }
+        public string Type { get; set; }
+    }
+}
